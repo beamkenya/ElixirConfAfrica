84a580062da4dd97b542f4c450e12b49c29b3c18
okoth <evansokoth@remote.com>
2023-08-10 00:10:10 +0300

    maybe add dialyzer

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule ElixirConfAfrica.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
   end
 
