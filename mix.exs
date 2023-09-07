6c12ae254a397f4b793971b6f0b8335a6e01a2d7
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-09-07 05:56:25 +0300

    Generated gen auth files

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule ElixirConfAfrica.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.7"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
