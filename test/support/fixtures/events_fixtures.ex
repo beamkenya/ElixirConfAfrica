efe5121d5a3a007ec878be659ee7dcc2913f3e5f
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-10-30 07:22:02 +0300

    Used test libraries

--- test/support/fixtures/events_fixtures.ex
+++ test/support/fixtures/events_fixtures.ex
@@ -22,4 +22,47 @@ defmodule ElixirConfAfrica.EventsFixtures do
 
     event
   end
+
+  def elixir_conf_event_fixture(attrs \\ %{}) do
+    {:ok, event} =
+      attrs
+      |> Enum.into(%{
+        name: "ElixirConf Africa 2024",
+        description: "some description",
+        location: "some location",
+        event_type: "some event_type",
+        start_date: ~N[2023-10-05 06:18:00],
+        end_date: ~N[2023-10-05 06:18:00]
+      })
+      |> ElixirConfAfrica.Events.create_event()
+
+    event
+  end
+
+  def elixir_conf_event_with_tickets_fixture(attrs \\ %{}) do
+    {:ok, event} =
+      attrs
+      |> Enum.into(%{
+        name: "ElixirConf Africa 2024",
+        description: "some description",
+        location: "some location",
+        event_type: "some event_type",
+        start_date: ~N[2023-10-05 06:18:00],
+        end_date: ~N[2023-10-05 06:18:00]
+      })
+      |> ElixirConfAfrica.Events.create_event()
+
+    {:ok, ticket_type} =
+      attrs
+      |> Enum.into(%{
+        event_id: event.id,
+        name: "some name",
+        description: "some description",
+        price: "120.5",
+        number: "357"
+      })
+      |> ElixirConfAfrica.TicketTypes.create_ticket_type()
+
+    [event, ticket_type]
+  end
 end
