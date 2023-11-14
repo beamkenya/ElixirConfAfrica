4dfbf7c5d06a974e495e7869a03ea0cc13b7f89f
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-11-14 18:46:13 +0300

    Removed static id of 1 from event map

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -4,11 +4,10 @@ defmodule ElixirConfAfrica.Factory do
 
   def build(:elixir_conf_event) do
     %ElixirConfAfrica.Events.Event{
-      id: 1,
       name: "ElixirConf Africa 2024",
-      description: "some description",
-      location: "some location",
-      event_type: "some event_type",
+      description: "description",
+      location: "location",
+      event_type: "event_type",
       start_date: ~N[2023-10-05 06:18:00],
       end_date: ~N[2023-10-05 06:18:00]
     }
