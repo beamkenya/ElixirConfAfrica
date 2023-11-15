207c6a6add887da31f20be6db7803d002a62e567
okoth <evansokoth@remote.com>
2023-11-15 11:00:47 +0300

    be more stricter

--- .credo.exs
+++ .credo.exs
@@ -81,7 +81,7 @@
         #
         ## Readability Checks
         #
-        {Credo.Check.Readability.AliasOrder, false},
+        {Credo.Check.Readability.AliasOrder, []},
         {Credo.Check.Readability.FunctionNames, []},
         {Credo.Check.Readability.LargeNumbers, []},
         {Credo.Check.Readability.MaxLineLength, [priority: :low, max_length: 120]},
